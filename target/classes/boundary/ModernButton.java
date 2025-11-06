a4f63fc2720422c86f8ac84bf3120f07fb247bf7
Francesco Cervera <francescocervera2005@gmail.com>
2025-11-06 14:26:47 +0100

    Add files via upload

---
+++ target/classes/boundary/ModernButton.java
@@ -0,0 +1,23 @@
+package Boundary;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+
+public class ModernButton 
+{
+	public static JButton createNewButtonPainted(String textArea, Color colorButton, Color colorText, Color colorBorder) 
+    { 
+        JButton nuovoBottone = new JButton(textArea);
+        nuovoBottone.setBackground(colorButton);
+        nuovoBottone.setFont(new Font("Arial", Font.BOLD, 14));
+        nuovoBottone.setForeground(colorText);
+        nuovoBottone.setBorder(BorderFactory.createLineBorder(colorBorder, 2));
+        nuovoBottone.setPreferredSize(new Dimension(160, 40)); // puoi aumentare questi valori
+        nuovoBottone.setFocusPainted(false);
+        return nuovoBottone;
+    }
+}
