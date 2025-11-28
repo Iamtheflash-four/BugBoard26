bbf0e012391d2ead6f64836e47c8aed7edcdbd94
Francesco Cervera <francescocervera2005@gmail.com>
2025-11-28 11:10:43 +0100

    Modifiche funzionali
    Modifica visibilità alcuni attributi per renderli visibili alla sottoclasse BarraUtenteAdmin
    
    Rimozione controllo status di admin

--- src/main/java/boundary/BarraUtente.java
+++ src/main/java/boundary/BarraUtente.java
@@ -1,20 +1,8 @@
 package boundary;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-
-import javax.swing.Box;
-import javax.swing.BoxLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollBar;
-import javax.swing.JScrollPane;
+import java.awt.*;
+import javax.swing.*;
 import javax.swing.border.MatteBorder;
-
 import boundary.theme.ModernButton;
 import boundary.theme.ModernLabel;
 import controller.AreaUtenteController;
@@ -22,79 +10,96 @@ import entity.Utente;
 
 public class BarraUtente extends JPanel
 {
-	private AreaUtenteController controller;
-	private Utente utente;
-	private JPanel titlePanel;
-	private JLabel title;
-	private JButton segnalaIssueButton;
-	private JPanel buttonPanel;
-	private JButton logoutButton;
-	private JButton profiloButton;
-	
-	public BarraUtente(Utente utente, AreaUtenteController controller)
-	{
-		super();
-		this.controller = controller;
-		this.utente = utente;
-		setSize(Integer.MAX_VALUE, 70);
-		setMaximumSize(new Dimension(Integer.MAX_VALUE, 70));
-		setPreferredSize(new Dimension(0, 70));
-		this.setAlignmentX(CENTER_ALIGNMENT);
-		setBackground(Color.WHITE);
-		setBorder(new MatteBorder(0, 0, 2, 0, new Color(200,200,200)));
+    private AreaUtenteController controller;
+    private Utente utente;
+    private JLabel title;
+    private JButton segnalaIssueButton;
+    protected JPanel buttonPanel;
+    private JButton logoutButton;
+    private JButton profiloButton;
 
-		
-		title = ModernLabel.createTitleLabel("Benvenuto " + utente.getNome());
-		title.setPreferredSize(new Dimension(300, 30));
-		
-		segnalaIssueButton = ModernButton.createNavbarButton("<html>Segnala<br>issue</html>");
-		logoutButton = ModernButton.createNavbarButton("Logout");
-		profiloButton = ModernButton.createNavbarButton("Profilo 👤");
-		
-		addButtonEvents();
-		createTitlePanel();
-	}
+    private JButton gestioneIssueButton;  // Sempre visibile
 
-	private void createTitlePanel() 
-	{
-		buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-		buttonPanel.setBackground(Color.WHITE);
-		buttonPanel.setVisible(true);
-		
-		buttonPanel.add(segnalaIssueButton);
-		buttonPanel.add(logoutButton);
-		buttonPanel.add(profiloButton);
-			
-		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-		add(title);
-		add(buttonPanel);
-	}
-	
-	private void addButtonEvents()
-	{
-		addSegnalaIssueEvent();
-		addProfiloEvent();
-		addLogoutEvent();
-	}
+    public BarraUtente(Utente utente, AreaUtenteController controller)
+    {
+        super();
+        this.controller = controller;
+        this.utente = utente;
+        setSize(Integer.MAX_VALUE, 70);
+        setMaximumSize(new Dimension(Integer.MAX_VALUE, 70));
+        setPreferredSize(new Dimension(0, 70));
+        setAlignmentX(CENTER_ALIGNMENT);
+        setBackground(Color.WHITE);
+        setBorder(new MatteBorder(0, 0, 2, 0, new Color(200,200,200)));
 
-	private void addSegnalaIssueEvent() {
-		segnalaIssueButton.addActionListener(e->{
-			controller.segnalaIssue(utente);
-		});
-	}
-	
-	private void addProfiloEvent() 
-	{
-		profiloButton.addActionListener(e->{
-			controller.showProfilo(utente);
-		});
-	}
+        title = ModernLabel.createTitleLabel("Benvenuto " + utente.getNome());
+        title.setPreferredSize(new Dimension(300, 30));
 
-	
-	private void addLogoutEvent()
-	{
-		logoutButton.addActionListener(e->{
-			controller.switchLogin();
-		});
-	}
+        segnalaIssueButton = ModernButton.createNavbarButton("<html>Segnala<br>issue</html>");
+        logoutButton = ModernButton.createNavbarButton("Logout");
+        profiloButton = ModernButton.createNavbarButton("Profilo 👤");
+
+        gestioneIssueButton = ModernButton.createNavbarButton("Gestione Issue");
+        gestioneIssueButton.setPreferredSize(new Dimension(160, 30));
+
+    
+
+        addButtonEvents();
+        createTitlePanel();
+    }
+
+    private void createTitlePanel() 
+    {
+        buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT, 14, 0));
+        buttonPanel.setBackground(Color.WHITE);
+
+        buttonPanel.add(segnalaIssueButton);
+
+        // Bottone sempre visibile
+        buttonPanel.add(gestioneIssueButton);
+
+      
+
+        buttonPanel.add(logoutButton);
+        buttonPanel.add(profiloButton);
+
+        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+        add(title);
+        add(Box.createHorizontalGlue());
+        add(buttonPanel);
+    }
+
+    private void addButtonEvents()
+    {
+        addSegnalaIssueEvent();
+        addGestioneIssueEvent();
+        addProfiloEvent();
+        addLogoutEvent();
+      
+    }
+
+    private void addSegnalaIssueEvent() {
+        segnalaIssueButton.addActionListener(e->{
+            controller.segnalaIssue(utente);
+        });
+    }
+
+    private void addGestioneIssueEvent() {
+        //da impl
+
+    }
+
+    private void addProfiloEvent() {
+        profiloButton.addActionListener(e->{
+            controller.showProfilo(utente);
+        });
+    }
+
+    private void addLogoutEvent() {
+        logoutButton.addActionListener(e->{
+            controller.switchLogin();
+        });
+    }
+
+  
 }
