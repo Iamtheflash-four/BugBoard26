5390efcd82ae2972a92b13a7b0af156aa8e0c85b
Francesco Cervera <francescocervera2005@gmail.com>
2025-11-28 11:15:31 +0100

    Modifica funzionale
    Controllo status admin per richiamare la giusta GUI

--- src/main/java/controller/AreaUtenteController.java
+++ src/main/java/controller/AreaUtenteController.java
@@ -13,7 +13,12 @@ public class AreaUtenteController extends Controller
 		super(controller);
 		this.utente = user;
 		
-		frame = new boundary.PersonalAreaUtente(this, utente);
+		if(utente.getAdminStatus())
+		{
+			frame = new boundary.PersonalAreaUtenteAdmin(this, utente);
+
+		}
+		else frame = new boundary.PersonalAreaUtente(this, utente); 
 		
 	}
 	
@@ -23,7 +28,10 @@ public class AreaUtenteController extends Controller
 	
 	public static void main(String[] args)
 	{
-		Utente utente = new Utente(1,"Sasy", "Correra", "s.correra@studenti.unina.it", "nooo", true, "shdfh");
+		Utente utente = new Utente(1,"Sasy", "Correra", "s.correra@studenti.unina.it", "RomaMerda", "shdfh", true);
+		Utente utente2 = new Utente(2, "Francy", "Cervera", "f.cervera@studenti.unina.it", "Forzainter", "lll", true);
+		Utente provaNotAdmin = new Utente (3, "Adolf", "Hitler", "ad.hilter@vivailduce.de", "DuceAlè", "vljfjdbf", false);
+		
 		new AreaUtenteController(new Controller(), utente);
 	}
 
