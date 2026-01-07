87077302489d2fb3b6f43ae1f6598a7f4e9eaa95
Francesco Cervera <francescocervera2005@gmail.com>
2026-01-07 20:18:39 +0100

    Add files via upload

--- src/main/java/entity/Issue.java
+++ src/main/java/entity/Issue.java
@@ -1,19 +1,24 @@
 package entity;
 
+import java.time.LocalDate;
 import java.util.Date;
+
+import jakarta.json.bind.annotation.JsonbDateFormat;
 
 public class Issue 
 {
+	private long idProgetto;
 	private String progetto;
 	private String tipo;
 	private String priorita;
 	private String titolo;
 	private String descrizione;
-	private Date data;
-	
+	@JsonbDateFormat("yyyy-MM-dd")
+	private LocalDate data;	
 
-	public Issue(String progetto, String tipo, String priorita, String titolo, String descrizione, Date data) {
+	public Issue(long idProgetto, String progetto, String tipo, String priorita, String titolo, String descrizione, LocalDate data) {
 		super();
+		this.idProgetto = idProgetto;
 		this.progetto = progetto;
 		this.tipo = tipo;
 		this.priorita = priorita;
@@ -48,11 +53,11 @@ public class Issue
 		return descrizione;
 	}
 
-	public Date getData() {
+	public LocalDate getData() {
 		return data;
 	}
 
-	public void setData(Date data) {
+	public void setData(LocalDate data) {
 		this.data = data;
 	}
 
@@ -71,4 +76,13 @@ public class Issue
 	public void setDescrizione(String descrizione) {
 		this.descrizione = descrizione;
 	}
+
+	public long getIdProgetto() {
+		return idProgetto;
+	}
+
+	public void setIdProgetto(long idProgetto) {
+		this.idProgetto = idProgetto;
+	}
+	
 }
